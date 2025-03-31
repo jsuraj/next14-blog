695bafd3e4f4fa69bc1c7138d9de6c4a0ed942a6
Suraj Jagtap <jsurajbaramati@gmail.com>
2025-04-01 00:14:15 +0530

    runtime fix for svix

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,6 +3,10 @@ import { headers } from 'next/headers';
 import { WebhookEvent } from '@clerk/nextjs/server';
 import { db } from '@/db/db';
 
+export const config = {
+  runtime: 'nodejs',
+};
+
 export async function POST(req: Request) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET;
 
