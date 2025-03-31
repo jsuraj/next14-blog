2597663049aa0380ee1c30dfd6530da310aed97f
Suraj Jagtap <jsurajbaramati@gmail.com>
2025-04-01 00:28:51 +0530

    fixed headers

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,5 +1,4 @@
 import { Webhook } from 'svix';
-import { headers } from 'next/headers';
 import { WebhookEvent } from '@clerk/nextjs/server';
 import { db } from '@/db/db';
 
@@ -18,10 +17,9 @@ export async function POST(req: Request) {
   const wh = new Webhook(SIGNING_SECRET);
 
   // Get headers
-  const headerPayload = await headers();
-  const svix_id = headerPayload.get('svix-id');
-  const svix_timestamp = headerPayload.get('svix-timestamp');
-  const svix_signature = headerPayload.get('svix-signature');
+  const svix_id = req.headers.get('svix-id');
+  const svix_timestamp = req.headers.get('svix-timestamp');
+  const svix_signature = req.headers.get('svix-signature');
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
